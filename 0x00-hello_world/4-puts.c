be196c76c7d737ea5656070a0e59cac35256943e
Yobadagne <yobadagne@gmail.com>
2022-09-08 12:20:34 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *Function 
-
-
+ *Function - main
+ *
+ *return :0
  */
 int main(void)
 {
-	puts(" Programming is like building a multilingual puzzl/e");
+	puts("\" Programming is like building a multilingual puzzle\n");
 	return (0);
 } 
