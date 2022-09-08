2f73366ae0e73cc8200790526564f46d56e4006c
Yobadagne <yobadagne@gmail.com>
2022-09-08 12:48:11 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  *main - void
  *
- *Return: returns
+ *Return: returns Zero
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-} 
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
