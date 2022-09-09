a5c1a2c5ea697b555712af8f416ac1f4537d9c7f
Yobadagne <yobadagne@gmail.com>
2022-09-09 14:35:55 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - coid
+ *Return: zero
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
