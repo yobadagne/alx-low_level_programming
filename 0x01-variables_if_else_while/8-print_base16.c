6dce1cb9a5b5197ce4c7acd512b25ced9e8ba33a
Yobadagne <yobadagne@gmail.com>
2022-09-09 14:45:02 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@ int main(void)
 	int n;
 	char c;
 
-	for(n  =0; n <= 9; n++)
+	for (n = 0; n <= 9; n++)
 	{
 		putchar(n % 10 + '0');
 	}
-	for(c = 'a'; c <= 'f'; c++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
