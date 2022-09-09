a3b37ad923f932ecca5458a87a93c80a1e0a76e3
Yobadagne <yobadagne@gmail.com>
2022-09-09 13:31:04 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - void
+ *Return: zero
+ */
+int main(void)
+{
+    char c;
+    for (c = 'a'; c <= 'z'; c++)
+        putchar(c);
+    for (c = 'A'; c <= 'Z'; c++)
+	  putchar(c);
+    putchar('\n');
+    return 0;
+}
