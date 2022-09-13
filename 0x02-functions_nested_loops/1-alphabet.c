c18677b63341fb76cea5165f77215612ddb3c300
Yobadagne <yobadagne@gmail.com>
2022-09-13 12:33:49 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+        _putchar('\n');
+}
