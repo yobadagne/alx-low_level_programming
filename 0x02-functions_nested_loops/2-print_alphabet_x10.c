78b69f7de28d08b496793e00923bf4faa9f4f37d
Yobadagne <yobadagne@gmail.com>
2022-09-13 12:58:53 -0700

    task 1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints char
+ *Returns: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++){
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
