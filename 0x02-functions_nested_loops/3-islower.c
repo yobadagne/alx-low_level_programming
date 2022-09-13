ddd60a125452725780d2bbbcbab260697d8989e8
Yobadagne <yobadagne@gmail.com>
2022-09-13 13:39:30 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
  *_islower - lower case
+ *
  *Return: int
  */
 int _islower(int c)
 {
 	int r;
 	
-	if (c >= 65 && c <= 90)
-	{
-		r = 0;
-	}
-	else if (c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		r = 1;
+	}
+	else
+	{
+		r = 0;
 	}
 	return r;
 }
