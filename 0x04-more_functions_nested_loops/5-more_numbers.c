333a220fef51e487d9706e030c53f0fb980df7df
Yobadagne <yobadagne@gmail.com>
2022-09-15 14:13:40 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *more_numbers - prints
+ *
+ * Return: numbers
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
