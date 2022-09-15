defa17916cc60cfde346b6ecb899b56802089afe
Yobadagne <yobadagne@gmail.com>
2022-09-15 12:51:07 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+ *_isdigit - returns digit
+ *
+ * @c: integer
+ *
+ * Return: digit
+ */
+int _isdigit(int c)
+{
+	if (c <= 48 && c <= 59)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
