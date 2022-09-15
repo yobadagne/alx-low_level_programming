5d3a910ac7838b60fd1d5c237d8664328ab11048
Yobadagne <yobadagne@gmail.com>
2022-09-15 13:46:00 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_most_numbers - prints numbers
+ *
+ *Return: numbers
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
