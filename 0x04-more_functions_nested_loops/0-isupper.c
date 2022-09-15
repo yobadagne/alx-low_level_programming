2e0a59de4cde57040864bd3960fc4f811bec63d7
Yobadagne <yobadagne@gmail.com>
2022-09-15 12:34:43 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/**
+ *_isupper - gives upper case letter
+ *
+ * @c: character
+ *
+ * Return: upper char
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
