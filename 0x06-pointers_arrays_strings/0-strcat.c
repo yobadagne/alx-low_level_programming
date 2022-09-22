137c815cab495348ff8b0e4586a97064b5a106dd
Yobadagne <yobadagne@gmail.com>
2022-09-22 03:53:56 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ *_strcat - conc strings
+ *@dest: first
+ *@src: second string
+ *Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
