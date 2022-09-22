013d1911050f15aba6d5588d3815d201bd4fb18d
Yobadagne <yobadagne@gmail.com>
2022-09-22 05:52:34 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,5 +8,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest, src));
+	int i, j = 0;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	while (src[j] != '\0')
+	{
+	dest[i] = src[j];
+	j++;
+	i++;
+	}
+	return (dest);
 }
