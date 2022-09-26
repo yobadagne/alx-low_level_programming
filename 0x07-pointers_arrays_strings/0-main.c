8e931a0a8905062f576e9bf257c28b8452884f19
yobadagne <yobadagne@gmail.com>
2022-09-26 11:43:40 +0000

    task 0

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -15,20 +15,19 @@ void simple_print_buffer(char *buffer, unsigned int size)
         i = 0;
         while (i < size)
         {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+		printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+		}
+	printf("\n");
 }
-
 /**
  * main - check the code
  *
@@ -36,11 +35,11 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char buffer[98] = {0x00};
+	char buffer[98] = {0x00};
 
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
+	simple_print_buffer(buffer, 98);
+	 _memset(buffer, 0x01, 95);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);    
+	return (0);
 }
