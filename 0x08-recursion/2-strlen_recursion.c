ef77867c23b05a315509e172c4840ae2603a9a52
yobadagne <yobadagne@gmail.com>
2022-09-29 11:53:47 +0000

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*_strlen_recursion - num of char
+*@s: string
+*Return: sum
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else 
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
