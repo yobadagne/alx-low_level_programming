83731c6e58c9ffba8718420eb1c8c9e885f16d50
yobadagne <yobadagne@gmail.com>
2022-09-29 12:10:12 +0000

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,11 +6,10 @@
 #include <stdio.h>
 int factorial(int n)
 {
-	if (n < 0) 
+	if (n < 0)
 		return (-1);
-	else
-	{	if (n == 1)
-			return (1) ;
-		return ( n * factorial (n-1));
-	}
+	else if (n == 1)
+		return (1);
+	return (n * factorial (n - 1));
+	
 }
