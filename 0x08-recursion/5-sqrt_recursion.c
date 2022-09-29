8696b18b3e767fdddbac82973c3504802f82d5b9
yobadagne <yobadagne@gmail.com>
2022-09-29 14:22:06 +0000

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,9 @@
 #include "main.h"
-
 /**
  * _sqrt_recursion - find natural square root
  * @n: in
- *@val: val
  * Return: int
  */
-int square(int n, int val);
 int _sqrt_recursion(int n)
 {
 	return (square(n, 1));
