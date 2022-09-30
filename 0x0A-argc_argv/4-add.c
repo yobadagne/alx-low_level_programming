df2abe54f191c6d0d385ea34f8b377d36836e361
yobadagne <yobadagne@gmail.com>
2022-09-30 18:56:02 +0000

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*main - main function
-*@argc: count
-*@argv: vector
-*Return: sum
-*/
+ * isInteger - checks if s is an integer
+ * @s: string to check
+ * Return: 0 or 1
+ */
+
+int isInteger(const char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (1);
+	i++;
+}
+return (0);
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
 int main(int argc, char const *argv[])
 {
-	int i, sum = 0;
+int sum = 0;
+while (--argc)
+{
+	if (isInteger(argv[argc]))
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum += atoi(argv[argc]);
+}
 
-	if (argc == 1)
-	{
-		printf("%i\n", 0);
-		return (0);
-	}
-	for (i = 1; i <= argc - 1; i++)
-	{
-		if (*argv[i] < 48 || *argv[i] > 57)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[i]);
-	}
-	printf("%d\n", sum);
-	return (0);
+printf("%i\n", sum);
+
+return (0);
 }
