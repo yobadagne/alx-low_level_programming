73d02e76018b6f2ad4633dd35ec61f3be830ea87
yobadagne <yobadagne@gmail.com>
2022-09-30 16:02:54 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - main
+*@argc: argument
+*@argv: array
+*Return: zero
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n",(argc-1));
+	return(0);
+}
