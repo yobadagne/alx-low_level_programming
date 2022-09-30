94e5da4a27caa40d60f266785283f6f8d2013c81
yobadagne <yobadagne@gmail.com>
2022-09-30 16:59:36 +0000

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
-* main - function
-* @argc: argument
-* @argv: array
-* Return: zero
-*/
+ * main - prints the number of arguments passed into the program
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+
 int main(int argc, char const *argv[])
 {
 	(void)argv;
 	printf("%i\n", argc - 1);
-	return(0);
+	return (0);
 }
