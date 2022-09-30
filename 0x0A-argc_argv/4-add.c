2ec728e1a97351a318f83342f0776e44e56bf376
yobadagne <yobadagne@gmail.com>
2022-09-30 18:28:21 +0000

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main function
+*@argc: count
+*@argv: vector
+*Return: sum
+*/
+int main (int argc, char const *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%i\n", 0);
+		return (0);
+	}
+	for (i = 1; i <= argc - 1; i++)
+	{	
+		if (*argv[i] < 48 || *argv[i] > 57)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+printf("%d\n", sum);
+return (0);
+}
