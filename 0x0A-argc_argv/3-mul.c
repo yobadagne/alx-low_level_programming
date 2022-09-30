43c065bb13cb1cab6c38b6c3a4fc7d619b45f596
yobadagne <yobadagne@gmail.com>
2022-09-30 17:20:44 +0000

    task 2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - main
+*@argc: count
+*@argv: value
+*Return: one
+*/
+int main(int argc, char *argv[])
+{
+	int mul;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return(1);
+	}
+	mul = (atoi(argv[1]) * atoi(argv[2]));
+	printf("%i\n", mul);
+	return (0);
+}
