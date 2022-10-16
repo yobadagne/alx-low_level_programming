55daf013e9fde881b27235606ebe05db8f0ccc8e
yobadagne <yobadagne@gmail.com>
2022-10-16 20:15:34 +0000

    task 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%d", va_arg(args, int));
 
-			if (i != n - 1  && separator != NULL)		printf("%s", separator);
+			if (i != n - 1  && separator != NULL)
+				printf("%s", separator);
 
 			i++;
 		}
